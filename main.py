ced74d9a9c737a543f14e5c0689a2dbb66011518
Salmaan Saeed <salsaeed999@gmail.com>
2025-08-17 14:55:46 -0400

    implemented collision for the player and the tree objects

--- main.py
+++ main.py
@@ -6,6 +6,28 @@ class Player:
         self.pos = pos
         self.color = color
         self.speed = speed
+        self.vx = 0
+        self.vy = 0
+
+
+class Object:
+    def __init__(self, pos: pygame.Rect, color: pygame.Color):
+        self.pos = pos
+        self.color = color
+
+
+def collision(player: Player, tree: Object):
+    if player.pos.colliderect(tree.pos):
+        if player.vx > 0:
+            player.pos.x -= player.speed
+        if player.vx < 0:
+            player.pos.x += player.speed
+        if player.vy > 0:
+            player.pos.y -= player.speed
+        if player.vy < 0:
+            player.pos.y += player.speed
+
+    return player.pos
 
 
 def main():
@@ -23,6 +45,38 @@ def main():
     player_color = pygame.Color(150, 0, 0, 255)
     player = Player(player_pos, player_color, speed=2)
 
+    tree_pos = pygame.Rect((screen.get_width() / 4, screen.get_height() / 4), (25, 25))
+    tree_color = pygame.Color(30, 70, 0, 255)
+    tree1 = Object(tree_pos, tree_color)
+
+    tree_pos = pygame.Rect((screen.get_width() / 2, screen.get_height() / 4), (25, 25))
+    tree_color = pygame.Color(30, 70, 0, 255)
+    tree2 = Object(tree_pos, tree_color)
+
+    tree_pos = pygame.Rect(
+        (screen.get_width() * 3 / 4, screen.get_height() / 4), (25, 25)
+    )
+    tree_color = pygame.Color(30, 70, 0, 255)
+    tree3 = Object(tree_pos, tree_color)
+
+    tree_pos = pygame.Rect(
+        (screen.get_width() / 4, screen.get_height() * 3 / 4), (25, 25)
+    )
+    tree_color = pygame.Color(30, 70, 0, 255)
+    tree4 = Object(tree_pos, tree_color)
+
+    tree_pos = pygame.Rect(
+        (screen.get_width() / 2, screen.get_height() * 3 / 4), (25, 25)
+    )
+    tree_color = pygame.Color(30, 70, 0, 255)
+    tree5 = Object(tree_pos, tree_color)
+
+    tree_pos = pygame.Rect(
+        (screen.get_width() * 3 / 4, screen.get_height() * 3 / 4), (25, 25)
+    )
+    tree_color = pygame.Color(30, 70, 0, 255)
+    tree6 = Object(tree_pos, tree_color)
+
     while quit == False:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -30,13 +84,17 @@ def main():
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
-            player_pos.y -= player.speed
+            player.pos.y -= player.speed
+            player.vy = -player.speed
         if keys[pygame.K_s]:
-            player_pos.y += player.speed
+            player.pos.y += player.speed
+            player.vy = player.speed
         if keys[pygame.K_a]:
-            player_pos.x -= player.speed
+            player.pos.x -= player.speed
+            player.vx = -player.speed
         if keys[pygame.K_d]:
-            player_pos.x += player.speed
+            player.pos.x += player.speed
+            player.vx = player.speed
         if keys[pygame.K_ESCAPE]:
             quit = True
 
@@ -50,10 +108,27 @@ def main():
         if player.pos.y > screen.get_height() - player.pos.height:
             player.pos.y = screen.get_height() - player.pos.width
 
+        # collision
+        player.pos = collision(player, tree1)
+        player.pos = collision(player, tree2)
+        player.pos = collision(player, tree3)
+        player.pos = collision(player, tree4)
+        player.pos = collision(player, tree5)
+        player.pos = collision(player, tree6)
+
+        player.vx = 0
+        player.vy = 0
+
         # render loop
         screen.fill(screen_color)
 
         pygame.draw.rect(screen, player.color, player.pos, 0)
+        pygame.draw.rect(screen, tree1.color, tree1.pos, 0)
+        pygame.draw.rect(screen, tree2.color, tree2.pos, 0)
+        pygame.draw.rect(screen, tree3.color, tree3.pos, 0)
+        pygame.draw.rect(screen, tree4.color, tree4.pos, 0)
+        pygame.draw.rect(screen, tree5.color, tree5.pos, 0)
+        pygame.draw.rect(screen, tree6.color, tree6.pos, 0)
 
         pygame.display.flip()
 
